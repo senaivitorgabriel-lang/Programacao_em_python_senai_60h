3101af29e874b7ef7f5162a5b328131b51359a59
senaivitorgabriel-lang <senaivitorgabriel@gmail.com>
2026-02-26 17:24:04 -0300

    Add files via upload

---
+++ ALGORITIMO-FLUXOGRAMA/LINGUAGEM PYTHON/aula_2.py
@@ -0,0 +1,10 @@
+# interpretador vai ingnorar tudo que possuir uma # na frente
+
+
+# verificar se a instalação foi realizada corretamente
+
+
+# testando a linguagem e as ferramentas
+
+print ('Olá Mundo...')
+print ('Teste')
---
+++ ALGORITIMO-FLUXOGRAMA/LINGUAGEM PYTHON/dados_primitivos.py
@@ -0,0 +1,22 @@
+# dados primitivos em python
+
+# tipo string - texto
+'texto'
+"texto 2 dfgfdgvbdrbaz"
+
+# tipo interder - inteiro
+
+1
+2
+100
+
+# tipo float - real
+
+print(5.0 - 102.10)
+2.10
+100.
+0.5
+
+#tipo bool - lógico
+True
+False
