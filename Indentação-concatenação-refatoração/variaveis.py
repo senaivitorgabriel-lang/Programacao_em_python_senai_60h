97129fc0722b1cb8635db4105fc8c0e123085997
senaivitorgabriel-lang <senaivitorgabriel@gmail.com>
2026-03-05 15:54:21 -0300

    Add files via upload

---
+++ Indentação-concatenação-refatoração/variaveis.py
@@ -0,0 +1,64 @@
+#EXERCICIOS
+#Exercicio 1
+
+
+resultado = 17 % 4
+print(resultado)
+
+
+#Exercicio 2
+
+multipicacao = 9 * 3
+resultado = multipicacao ** 2 
+print(f"O resultado de (9 * 3) ** 2 é: {resultado}")
+
+
+#Exercicio 3
+
+num1 = 81
+resultado = num1 ** 0.5
+print(f"O resultado de 81 ** 0.5 {resultado}")
+
+#Exercicio 4
+
+num2 = 20
+soma = 3 * 4
+resultado = 3 * 4 
+print(f"O resultado é: {num2+soma}")
+
+#Exercicio 5
+
+multi = 15 * 2
+sub = 7
+print(f"O resultado {multi-sub}")
+
+#Exercicio 6
+
+resultado = 5 ** 3
+print(f"O resultado é: {resultado}")
+
+#Exercicio 7
+
+soma = 17 + 21 + 25
+dividir = 3
+print (f"O resultado é igual a: {soma/dividir}")
+
+#Exercico 8
+
+multip = 11 * 2
+adc = 7
+print(f"O resultado {multip+adc}")
+
+#Exercicio 9 
+
+multipl = 3 * 8
+sub2 = 15
+div2 = 2
+print(f"O resultado é igual a: {(multipl-sub2)/div2}")
+
+#Exercicio 10
+
+resultado = 2 ** 10
+print(f"O resultado é: {resultado}")
+
+      
