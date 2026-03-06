59e59eee247ef975d4e067e05db713f2781a0d6a
senaivitorgabriel-lang <senaivitorgabriel@gmail.com>
2026-03-06 14:34:11 -0300

    Add files via upload

---
+++ Pasta aula 7/aula_7.py
@@ -0,0 +1,28 @@
+#0
+
+numeros_pares = list(range(2, 21, 2))
+print(f"O resultado é igual a: {numeros_pares} ")
+
+#1
+
+numeros = list(range(1, 11))
+print(numeros)
+
+#2
+
+print(numeros[2])
+
+#3
+
+numeros.append(9)
+print(numeros)
+
+#4
+
+numeros.remove(5)
+print(numeros)
+
+#5
+
+carros = ["ferrari", "mercedes", "lamborghini"]
+print(carros)
